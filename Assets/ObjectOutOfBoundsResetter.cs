2db36316edce9cc3c35cc7fed716626c6ff56273
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-03-08 22:36:45 +0100

    Tricks and adjustments

---
+++ Assets/ObjectOutOfBoundsResetter.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectOutOfBoundsResetter : MonoBehaviour
+{
+    private void OnCollisionEnter(Collision other)
+    {
+        try
+        {
+            other.gameObject.GetComponent<IRestartable>().Restart();
+        }
+        catch (Exception e)
+        {
+            Debug.LogWarning(e.Message);
+        }
+    }
+}
