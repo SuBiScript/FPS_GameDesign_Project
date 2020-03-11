8ac7f0b29caaae11e32ad1cf7b32f4fda547f6b1
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-03-11 01:44:24 +0100

    Scene

---
+++ Assets/LightFlickering.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlickering : MonoBehaviour
+{
+    public float maxLightFlicker;
+
+    public float minLightFlicker;
+
+    public float currentFlicker;
+
+    public float maxFlickerDeviation;
+
+    public Light light;
+    public Color originalColor;
+
+    private System.Random randomGen = new System.Random(Guid.NewGuid().GetHashCode());
+
+    void Start()
+    {
+        light = GetComponent<Light>();
+        originalColor = light.color;
+    }
+
+    void Update()
+    {
+        
+    }
+}
