8ef799a693cb38f29f8393cc159de33211959314
Jordi Alba <44704875+jalba002@users.noreply.github.com>
2020-03-17 23:48:45 +0100

    Cube solving and removing references

--- Assets/Scripts/Controllers/GameController.cs
+++ Assets/Scripts/Controllers/GameController.cs
@@ -1,11 +1,8 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
 using UnityEngine.UI;
-using UnityEngine.SceneManagement;
-using UnityEngine.Events;
 
 public class GameController : Singleton<GameController>
 {
